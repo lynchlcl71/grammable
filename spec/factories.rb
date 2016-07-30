d33bbf52db13e38ec037a9ddcae52f0dc4914951
vagrant <vagrant@precise32.(none)>
2016-07-30 21:29:15 +0000

    Validated picture field on a gram

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,11 +5,11 @@ FactoryGirl.define do
     end
     password "secretPassword"
     password_confirmation "secretPassword"
-    
   end
-  
+
   factory :gram do
     message "hello"
+    picture { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'picture.png'), 'image/png') }
     association :user
   end
 end
