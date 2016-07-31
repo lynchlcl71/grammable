e9ae3ba04bd507989369de56481d04b639693a4a
vagrant <vagrant@precise32.(none)>
2016-07-31 04:55:12 +0000

    Added Support to Create Comments on Grams

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -2,6 +2,8 @@ class Gram < ActiveRecord::Base
   validates :message, presence: true
   validates :picture, presence: true
 
+  has_many :comments
+
   belongs_to :user
 
   mount_uploader :picture, PictureUploader
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -9,12 +9,12 @@ RSpec.describe CommentsController, type: :controller do
       sign_in u
 
       post :create, gram_id: p.id, comment: { message: 'awesome gram' }
-  
+      
       expect(response).to redirect_to root_path
       expect(p.comments.length).to eq 1
       expect(p.comments.first.message).to eq "awesome gram"
     end
-    
+
     it "should require a user to be logged in to comment on a gram" do
       p = FactoryGirl.create(:gram)
       post :create, gram_id: p.id, comment: { message: 'awesome gram' }
@@ -24,8 +24,11 @@ RSpec.describe CommentsController, type: :controller do
     it "should return http status code of not found if the gram isn't found" do
       u = FactoryGirl.create(:user)
       sign_in u
+      
       post :create, gram_id: 'YOLOSWAG', comment: { message: 'awesome gram' }
       expect(response).to have_http_status :not_found
     end
+
   end
+
 end
